7a70cb7033f3d01ee52968fbc15143694e406de8
Henry0406 <enoc0406@hotmail.com>
2023-03-09 18:19:56 -0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+import './App.css'
+import Board from './components/Board/Board';
+import ScoreBoard from './components/ScoreBoard/ScoreBoard';
+
+const winningPositions = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+];
+
+const App = () => {
+
+  const [turn, setTurn] = useState('X');
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [winningSquares, setWinningSquares] = useState([]);
+  const [score, setScore] = useState ({
+    X: 0,
+    O: 0,
+  });
+
+  const reset = () => {
+    setTurn('X');
+    setSquares(Array(9).fill(null));
+    setWinningSquares([]);
+
+  }
+
+  const checkForWinner = newSquares => {
+    for(let i=0; i<winningPositions.length; i++){
+      const [a,b,c] = winningPositions[i];
+      if(newSquares[a] && newSquares[a] === newSquares[b] && newSquares[a] === newSquares[c]){
+        //Se determina Ganador
+        endGame(newSquares[a], winningPositions[i]);
+        return
+      }
+    }
+
+    if(!newSquares.includes(null)){
+      //Hay empate
+      endGame(null, Array.from(Array(10).keys()));
+      return
+    }
+    setTurn(turn == 'X' ? 'O' : 'X');
+  }
+
+  const handleClick = square => {
+    let newSquares = [...squares];
+    newSquares.splice(square, 1, turn);
+    setSquares(newSquares);
+    checkForWinner(newSquares);
+  }
+
+  const endGame = (result, winningPositions) => {
+    setTurn(null);
+    if(result !== null){
+      setScore({
+        ...score,
+        [result]: score[result] +1,
+      })
+    }
+
+    setWinningSquares(winningPositions);
+    setTimeout(reset, 2000);
+    
+  }
+
+  return (
+    <div className="container">
+      <h1 className='titulo'>Bienvenido al Juego del Gato</h1>
+      <Board winningSquares={winningSquares} turn={turn} squares={squares} onClick={handleClick}/>
+      <h2>Puntuación</h2>
+      <ScoreBoard scoreO={score.O} scoreX={score.X} />
+    </div>
+  );
+}
+
+export default App;
